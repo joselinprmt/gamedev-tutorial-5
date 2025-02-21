91d376473299ec92641c3eb763c166807f449acf
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2025-02-21 12:21:58 +0700

    feat(main): implement dash

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,11 +5,13 @@ extends CharacterBody2D
 @export var jump_speed = -200
 
 var jump_count = 0
+var dash_speed = 4
+var is_dashing = false
 
 func _physics_process(delta):
 	velocity.y += delta * gravity
 
-	# Handle jump
+	# Handle jump & double jump: W, SPACE, UP arrow
 	if Input.is_action_just_pressed('ui_up') and jump_count == 1:
 		velocity.y = jump_speed
 		jump_count = 2
@@ -20,13 +22,30 @@ func _physics_process(delta):
 
 	if !Input.is_action_just_pressed('ui_up') and is_on_floor():
 		jump_count = 0
+	
+	var direction := Input.get_axis("ui_left", "ui_right")
+		
+	# Handle Dash: direction + right mouse
+	if Input.is_action_just_pressed('ui_dash'):
+		if direction and  !is_dashing:
+			start_dash()
 
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -walk_speed
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  walk_speed
+	# Handle walk and dash
+	if direction:
+		if is_dashing:
+			velocity.x =  direction * walk_speed * dash_speed
+		else:
+			velocity.x =  direction * walk_speed
 	else:
 		velocity.x = 0
 
 	# "move_and_slide" already takes delta time into account.
 	move_and_slide()
+
+func start_dash():
+	is_dashing = true
+	$DashTimer.connect("timeout", stop_dash)
+	$DashTimer.start()
+
+func stop_dash():
+	is_dashing = false
