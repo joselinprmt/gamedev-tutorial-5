9cb9ff6c32c32170e310f8d63a1850ffa2b649a2
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2025-02-21 13:25:54 +0700

    fix(main): gdscript formatting

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -10,54 +10,56 @@ var jump_count = 0
 var dash_speed = 4
 var is_dashing = false
 
+
 func _physics_process(delta):
 	velocity.y += delta * gravity
 
 	# Handle jump & double jump: W, SPACE, UP arrow
-	if Input.is_action_just_pressed('ui_up') and jump_count == 1:
+	if Input.is_action_just_pressed("ui_up") and jump_count == 1:
 		velocity.y = jump_speed
 		sprite_2d.animation = "double_jumping"
 		jump_count = 2
-		
-		
-	if Input.is_action_just_pressed('ui_up') and is_on_floor():
+
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_speed
 		sprite_2d.animation = "jumping"
 		jump_count = 1
 
-	if !Input.is_action_just_pressed('ui_up') and is_on_floor():
+	if !Input.is_action_just_pressed("ui_up") and is_on_floor():
 		sprite_2d.animation = "default"
 		jump_count = 0
-	
+
 	var direction := Input.get_axis("ui_left", "ui_right")
-		
+
 	# Handle Dash: direction + right mouse
-	if Input.is_action_just_pressed('ui_dash'):
-		if direction and  !is_dashing:
+	if Input.is_action_just_pressed("ui_dash"):
+		if direction and !is_dashing:
 			start_dash()
 
 	# Handle walk and dash
 	if direction:
 		if is_dashing:
-			velocity.x =  direction * walk_speed * dash_speed
+			velocity.x = direction * walk_speed * dash_speed
 			sprite_2d.animation = "dashing"
 		else:
-			velocity.x =  direction * walk_speed
+			velocity.x = direction * walk_speed
 	else:
 		velocity.x = 0
 
 	# "move_and_slide" already takes delta time into account.
 	move_and_slide()
-	
+
 	# Flip according to dirrection
 	var is_left = velocity.x < 0
 	sprite_2d.flip_h = is_left
+
 
 func start_dash():
 	is_dashing = true
 	if not $DashTimer.timeout.is_connected(stop_dash):
 		$DashTimer.connect("timeout", stop_dash)
 	$DashTimer.start()
+
 
 func stop_dash():
 	is_dashing = false
