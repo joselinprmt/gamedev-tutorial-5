79d0392c71d36e0c8320dd5a067dd613681e17ad
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2025-02-21 11:48:05 +0700

    feat(main): implent double jump, add binding input key wasd & space for movement

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,13 +2,24 @@ extends CharacterBody2D
 
 @export var gravity = 200.0
 @export var walk_speed = 200
-@export var jump_speed = -300
+@export var jump_speed = -200
+
+var jump_count = 0
 
 func _physics_process(delta):
 	velocity.y += delta * gravity
 
-	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
+	# Handle jump
+	if Input.is_action_just_pressed('ui_up') and jump_count == 1:
 		velocity.y = jump_speed
+		jump_count = 2
+		
+	if Input.is_action_just_pressed('ui_up') and is_on_floor():
+		velocity.y = jump_speed
+		jump_count = 1
+
+	if !Input.is_action_just_pressed('ui_up') and is_on_floor():
+		jump_count = 0
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -walk_speed
