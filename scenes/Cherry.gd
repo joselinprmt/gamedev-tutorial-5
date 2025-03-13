eb218f28b0c0be693ebe317d0391edf63c95d703
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2025-03-14 05:27:07 +0700

    feat(main): change terrain position, fix gdformat

--- scenes/Cherry.gd
+++ scenes/Cherry.gd
@@ -3,6 +3,7 @@ extends Area2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var sfx_collect: AudioStreamPlayer2D = $sfx_collect
 
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		sfx_collect.play()
